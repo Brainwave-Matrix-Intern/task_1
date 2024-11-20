b0b610f591fc5841464b2ab6bc96aee6794a0618
MahwishZa <154221769+MahwishZa@users.noreply.github.com>
2024-11-20 11:29:10 +0500

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex items-center">
-                <span className="text-4xl font-bold">MZ</span>
+                <span className="text-4xl font-bold">JD</span>
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-3xl">
-                <a href="https://www.linkedin.com/in/mahwish-zafar-1a63a7272/" target="_blank" rel="noopener noreferrer">
+                <a href="#" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin />
                 </a>
-                <a href="https://github.com/MahwishZa" target="_blank" rel="noopener noreferrer">
+                <a href="#" target="_blank" rel="noopener noreferrer">
                     <FaGithub />
                 </a>
-                <a href="mailto:mahwishza@gmail.com">
+                <a href="#">
                     <FaEnvelope />
                 </a>
             </div>
